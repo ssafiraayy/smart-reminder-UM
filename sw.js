becf73393b26941432e4a07770f4c58218938779
safiraa <tsabbitah.nabila.2509347@students.um.ac.id>
2026-04-30 10:22:49 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'smart-reminder-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Tambahkan file CSS atau Gambar kamu di sini jika ada, contoh:
+  // './style.css',
+  // './icon.png'
+];
+
+// Tahap Install: Menyimpan file ke dalam cache (memori browser)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Tahap Aktivasi: Menghapus cache lama jika ada update
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Tahap Fetch: Mengambil data dari cache supaya bisa dibuka tanpa internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
